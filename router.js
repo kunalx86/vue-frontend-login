63c1cc343ee82c7906c54b93f58834024ca3ef10
Kunal Joshi <mail.kunaljoshi@gmail.com>
2020-03-24 23:13:02 +0530

    Added untracked files.

---
+++ router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './src/components/Home.vue';
+// import Signup from './src/components/Signup.vue'
+// import Login from './src/components/Login.vue';
+
+Vue.use(Router);
+
+export default ({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    // {
+    //   path: '/signup',
+    //   name: 'Signup',
+    //   component: Signup
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'Login',
+    //   component: Login
+    // }
+  ]
+});
---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    token: String,
+  },
+  data() {
+    return {
+      msg: String,
+    }
+  },
+  methods: {
+    verify: function() {
+      fetch('http://localhost:3000/posts/', {
+        headers: {
+          'auth-token': this.token,
+          'Content-type': 'application/json',
+        },  
+      })
+        .then(response => {
+          console.log(response);
+          
+          if (response.status === 401) {
+            this.msg = 'Not authorized';
+          }
+          if (response.status === 400) {
+            this.msg = 'Wrong Token';
+          }
+        }) 
+        .catch(err => this.msg = err);
+    }
+  }, 
+  beforeMount() {
+    this.verify();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
